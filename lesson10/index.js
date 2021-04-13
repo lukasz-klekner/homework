0a3a0e3d03bde329be459e0889e982ac087267e1
Lukasz Klekner <lukaszklekner@gmail.com>
2021-04-13 13:07:19 +0200

    lesson10: added second exercise

--- lesson10/index.js
+++ lesson10/index.js
@@ -21,3 +21,52 @@ function doZaplaty(priceFor100Km, distance){
 function displayInfo(name, age, howManyChildren){
     return `${name} ma ${age} lata i ${howManyChildren} dzieci`
 }
+
+/* zadanie 2 */
+function returnArray(array){
+    return array;
+}
+
+function returnSecondItem(array){
+    return array[1];
+}
+
+function returnInfoAboutArray(array){
+    return `Przekazana tablica ma ${array.length} elementów, a elementem nr 3 jest ${array[2]}`
+}
+
+function addElements(item){
+    return emptyArray.push(item);
+}
+
+const emptyArray = [];
+addElements('swinka')
+
+function displayPersonName({name}){
+    return name
+}
+
+const person = {
+    name: 'Lukasz',
+    surname: 'Klekner',
+    age: 26,
+    height: 172,
+}
+
+displayPersonName(person);
+
+function displayInfoAboutPerson({name, surname, age, height}){
+    return `${name} ${surname} ma ${age} lat
+    i ${height} cm wzrostu`
+}
+
+displayInfoAboutPerson(person);
+
+function changeAge(person, ageToAdd){
+    return { ...person,
+             age: ageToAdd
+            }
+}
+
+const newPerson = changeAge(person, 28);
+console.table(newPerson);
