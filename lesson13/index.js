41b7706e9487d05b64c950ffd6c86c05a81edd62
Lukasz Klekner <lukaszklekner@gmail.com>
2021-04-22 12:46:18 +0200

    lesson13: added ex.1

---
+++ lesson13/index.js
@@ -0,0 +1,8 @@
+/* zadanie 1 */
+function info(marka, model, rok_produkcji){
+    return console.log(`Samochód ${marka} ${model} - ${rok_produkcji}`)
+}
+
+function info(car){
+    return console.log(`Samochód ${car.marka} ${car.model} - ${car.rok_produkcji}`)
+}
