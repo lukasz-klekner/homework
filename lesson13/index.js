c9a60870c446a4acb3ba2ca66def1e22ea46542f
Lukasz Klekner <lukaszklekner@gmail.com>
2021-04-22 12:51:24 +0200

    lesson13: added ex.2

--- lesson13/index.js
+++ lesson13/index.js
@@ -3,6 +3,15 @@ function info(marka, model, rok_produkcji){
     return console.log(`Samochód ${marka} ${model} - ${rok_produkcji}`)
 }
 
-function info(car){
+function info2(car){
     return console.log(`Samochód ${car.marka} ${car.model} - ${car.rok_produkcji}`)
 }
+
+/* zadanie 2 */
+const car = {
+    marka : "Volkswagen",
+    model : "Golf",
+    rok_produkcji: 1985,
+}
+
+const {marka, model, rok_produkcji} = car;
