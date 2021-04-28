f7bee38d2abbb6760867c925d942d41889c7da67
Lukasz Klekner <lukaszklekner@gmail.com>
2021-04-28 18:28:57 +0200

    lesson15: added ex.1

---
+++ lesson15/index.js
@@ -0,0 +1,28 @@
+/* zadanie 1 */
+for(let i=1; i < 10; i++){
+    console.log(i);
+}
+
+for(let i=9; i > 0; i--){
+    console.log(i);
+}
+
+for(let i=5; i < 16; i++){
+    console.log(i);
+}
+
+for(let i=0; i > -11; i--){
+    console.log(i);
+}
+
+for(let i=1; i <= 20; i+=2){
+    console.log(i);
+}
+
+for(let i=10; i >= -10; i-=4){
+    console.log(i);
+}
+
+for(let i=1; i <= 100; i*=2){
+    console.log(i);
+}
